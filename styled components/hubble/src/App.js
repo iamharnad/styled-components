e9d81cd9d389ec29d9d8a3f2fb648160b740bf5d
Krishna Haranath <haranathpriyan@gmail.com>
2021-10-11 21:30:44 +0530

    first commit

---
+++ styled components/hubble/src/App.js
@@ -0,0 +1,11 @@
+import { Container } from './components/styles/Container.styled';
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello world</h1>
+    </Container>
+  );
+}
+
+export default App;
