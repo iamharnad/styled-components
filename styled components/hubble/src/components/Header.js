be570538a934f52e4cc5fb79f491feb7036bc887
Krishna Haranath <haranathpriyan@gmail.com>
2021-10-13 21:29:00 +0530

    fourth commit

--- styled components/hubble/src/components/Header.js
+++ styled components/hubble/src/components/Header.js
@@ -1,9 +1,38 @@
-import { StyledHeader } from './styles/Header.styled';
+import { StyledHeader, Nav, Logo, Image } from './styles/Header.styled';
+import { Container } from './styles/Container.styled';
+import { Button } from './styles/Button.styled';
+import { Flex } from './styles/Flex.styled';
 
 export default function Header() {
   return (
     <StyledHeader>
-      <h1>Hubble</h1>
+      <Container >
+      <Nav>
+        <Logo src='./images/logo.svg' alt='' />
+        <Button>
+          Try it for free
+        </Button>
+      </Nav>
+      <Flex>
+        <div>
+          <h1>
+            Build the community that fans will love!
+          </h1>
+          <p>
+            Huddle re-imagines the way we build communities. You
+            have a voice,
+            but so does your audience, create connections with your
+             users as
+              you engange in genuine discussion.
+          </p>
+          <Button bg='#ff0099' color='#fff'>
+            Get started for free
+          </Button>
+        </div>
+
+        <Image src='./images/illustration-mockups.svg' alt='' />
+      </Flex>
+      </Container>
     </StyledHeader>
   );
 }
