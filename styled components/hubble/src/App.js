a3abceb706d2c655cd39122dc64835c8ec8c6e51
Krishna Haranath <haranathpriyan@gmail.com>
2021-10-16 04:38:00 +0530

    fifth commit

--- styled components/hubble/src/App.js
+++ styled components/hubble/src/App.js
@@ -1,7 +1,10 @@
 import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Card from './components/Card';
 import { Container } from './components/styles/Container.styled';
 import GlobalStyles from './components/styles/Global';
+import content from './content';
 
 const theme = {
   colors: {
@@ -18,8 +21,11 @@ function App() {
       <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello world</h1>
+         {content.map((item, index) => (
+           <Card key={index} item={item}/>
+         ))}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
